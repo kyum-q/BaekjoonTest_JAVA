9ef68e6b71b9371689fa78caa267dc69fa248cae
김경미 <109158497+kyum-q@users.noreply.github.com>
2023-07-16 16:29:39 +0900

    [Bronze I] Title: 평균, Time: 260 ms, Memory: 19708 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,23 +1,29 @@
-
-import java.util.*;
+import java.io.*;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String [] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		double [] score = new double [N];
-		int max = 0;
-		for(int i=0;i<N;i++) {
-			score[i] = sc.nextInt();
-			if(max<score[i])
-				max = (int)score[i];
-		}
-		double sum =0;
-		for(int i=0;i<N;i++) {
-			score[i] = score[i]/max * 100;
-			sum += score[i];
-		}
-		double avg = sum/N;
-		System.out.println(avg);
-	}
+   public static void main (String [] args) throws IOException {
+       Scanner sc = new Scanner(System.in);
+       // N 입력
+       int N = sc.nextInt();
+
+       int [] score = new int[N];
+       double maxScore = 0;
+       for(int i=0;i<N;i++) {
+           // 실제 성적 입력
+           score[i] = sc.nextInt();
+           // 최고점 알아내기
+           if(maxScore < score[i])
+               maxScore = score[i];
+       }
+
+       // 점수 재계산 및 합치기
+       double sum = 0;
+       for(int i=0;i<N;i++) {
+           sum += score[i]/maxScore * 100.0;
+       }
+
+       // 평균 구하고 출력
+       System.out.println(sum/N);
+   }
 }
