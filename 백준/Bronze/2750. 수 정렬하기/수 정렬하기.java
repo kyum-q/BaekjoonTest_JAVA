4ac9e29402e04cf0d26b6ec30a1e94ce107b9bdc
김경미 <109158497+kyum-q@users.noreply.github.com>
2023-07-28 01:07:15 +0900

    [Bronze II] Title: 수 정렬하기, Time: 160 ms, Memory: 16240 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,37 +1,25 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String [] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		// N 입력
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		
-		// 입력받을 배열
-		int num [] = new int[N];
-		
-		// 배열 입력 
-		for(int i=0;i<N;i++) {
-			st = new StringTokenizer(br.readLine());
-			num[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		// 배열 정렬
-		Arrays.sort(num);
-		
-		// 출력
-		for(int i=0;i<N;i++)
-			bw.write(num[i]+"\n");
-		
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // N 입력
+        int N = Integer.parseInt(br.readLine());
+
+        int [] nums = new int [N];
+
+        for(int i=0;i<N;i++){
+            nums[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(nums);
+
+        for(int i=0;i<N;i++)
+            bw.write(nums[i]+"\n");
+        bw.flush();
+        bw.close();
+    }
 }
