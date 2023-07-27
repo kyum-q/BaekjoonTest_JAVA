e3d3c45b4c14de2e935dd1f2044a3e076cfa24bd
김경미 <109158497+kyum-q@users.noreply.github.com>
2023-07-28 00:24:31 +0900

    [Silver IV] Title: 카드2, Time: 188 ms, Memory: 45532 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,34 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import org.w3c.dom.Node;
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String [] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		// N 입력
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		
-		// 1~N까지 Queue에 삽입
-		Queue<Integer> queue = new LinkedList<Integer>();
-		for(int i=1;i<=N;i++) 
-			queue.add(i);
-		
-		// Queue에 하나만 남을 때까지 반복
-		while(queue.size() > 1) {
-			// 맨위에 있는 수 꺼내기
-			queue.poll();
-			
-			// 맨위에 있는 수 꺼내서 맨뒤에 삽입
-			int reInsert = queue.poll();
-			queue.add(reInsert);
-		}
-		
-		// 출력
-		System.out.println(queue.poll());
-	}
+    public static void main(String [] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Deque<Integer> deque = new LinkedList<>();
+
+        // N 입력
+        int N = Integer.parseInt(br.readLine());
+
+        // 수 입력
+        for(int i=0;i<N;i++) {
+            deque.add(i+1);
+        }
+
+        // 카드 지우고 넘기고 반복
+        while(deque.size() > 1) {
+            deque.pop();
+            deque.addLast(deque.pop());
+        }
+
+        System.out.println(deque.pop());
+    }
 }
