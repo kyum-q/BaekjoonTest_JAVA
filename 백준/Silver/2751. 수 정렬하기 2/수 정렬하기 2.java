68e2825f89dba20ad4bfa65f0df230930b59003f
김경미 <109158497+kyum-q@users.noreply.github.com>
2023-07-29 21:53:07 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1048 ms, Memory: 110892 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,8 +1,8 @@
 import java.io.*;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
+    public static int [] nums, temp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -11,18 +11,68 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
 
         // 수 입력
-        int [] nums = new int [N];
+        nums = new int [N];
+        temp = new int [N];
 
         for(int i=0;i<N;i++) {
             nums[i] = Integer.parseInt(br.readLine());
         }
 
-        Arrays.sort(nums);
+        // 수 정렬하기
+//        Arrays.sort(nums);
+
+        // 합병 정렬로 정렬하기
+        mergeSort(0, N-1);
 
         for(int i=0;i<N;i++) {
             bw.write(nums[i] + "\n");
         }
         bw.flush();
         bw.close();
+    }
+
+    public static void mergeSort(int s, int e) {
+        if(e - s < 1) {
+            return;
+        }
+
+        // 분할
+        int m = s + (e-s) / 2;
+        mergeSort(s, m);
+        mergeSort(m+1, e);
+
+        // s~e까지 temp 복사
+        for(int i=s;i<=e;i++) {
+            temp[i] = nums[i];
+        }
+
+        // 변수 초기화
+        int originalIndex = s;
+        int index1 = s;
+        int index2 = m+1;
+
+        // 병합
+        while(index1 <= m && index2 <= e) {
+            if(temp[index1] < temp[index2]) {
+                nums[originalIndex] = temp[index1];
+                index1++;
+            }
+            else {
+                nums[originalIndex] = temp[index2];
+                index2++;
+            }
+            originalIndex++;
+        }
+
+        while(index1 <= m) {
+            nums[originalIndex] = temp[index1];
+            index1++;
+            originalIndex++;
+        }
+        while(index2 <= e) {
+            nums[originalIndex] = temp[index2];
+            index2++;
+            originalIndex++;
+        }
     }
 }
