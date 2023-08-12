11ed6f9b9a36f5e098d914ef02602c8032ceea79
김경미 <109158497+kyum-q@users.noreply.github.com>
2023-08-12 20:37:43 +0900

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -10,17 +10,21 @@ public class Main {
 
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-
-        int count = 0;
-
+        
+        // 코인 입력
         int coins [] = new int[N];
 
         for(int i=0;i<N;i++) {
             coins[i] = Integer.parseInt(br.readLine());
         }
-        
+
+        int count = 0;
+
+        // 가장 큰 수부터 확인하면서 반복
         for(int i=N-1;i>=0;i--) {
+            // 만약 현재 돈이 해당 코인으로 나누어진다면
             if(K / coins[i] > 0) {
+                // count 증가 및 k에서 코인으로 계산한 금액 제외
                 int newCount = K / coins[i];
                 K = K - (newCount * coins[i]);
                 count += newCount;
