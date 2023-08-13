4d57d09f9144e4d8bef309feeba331db02f13132
김경미 <109158497+kyum-q@users.noreply.github.com>
2023-08-13 23:37:00 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 268 ms, Memory: 19208 KB -BaekjoonHub

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
@@ -11,20 +11,25 @@ public class Main {
 
         // 배열 생성
         boolean[] check = new boolean[range];
+        
         for (long i = 2; i * i<= max; i++) {
             // 제곱 수
             long pow = i * i;
+            // MIN보다 큰 제곱 수 알아내기
             long newPow = pow * Math.max((min / pow), 1);
 
+            // 만약 새로 구한 제곱 수도 min보다 작을 경우 제곱 한번 더 더하기
             if (newPow < min) {
-                newPow += i * i;
+                newPow += pow;
             }
             
+            // max 값까지 제곱수의 배수 확인하기
             for(long j=newPow;j<=max;j+=pow) {
                 check[(int) (j - min)] = true;
             }
         }
 
+        // 개수 알아내기
         int count = 0;
         for (int i = 0; i < range; i++) {
             if (!check[i]) {
