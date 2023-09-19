131e99eeecb746858d57c3f8ffde2232e9521442
김경미 <109158497+kyum-q@users.noreply.github.com>
2023-09-19 19:53:18 +0900

    [Silver I] Title: 단절점과 단절선, Time: 536 ms, Memory: 65584 KB -BaekjoonHub

--- 백준/Silver/14675. 단절점과 단절선/단절점과 단절선.java
+++ 백준/Silver/14675. 단절점과 단절선/단절점과 단절선.java
@@ -17,7 +17,6 @@ public class Main {
         }
 
         // 간선 입력
-        ArrayList<Node> lines = new ArrayList<>();
         for(int i=0;i<N-1;i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -26,7 +25,6 @@ public class Main {
 
             lists[a].add(b);
             lists[b].add(a);
-            lines.add(new Node(a,b));
         }
 
         // 질문 개수 Q 입력
@@ -41,14 +39,17 @@ public class Main {
 
             // k가 단절점인지 체크
             if(t == 1) {
+                // 해당 점이 2개 이상과 연결되 있는 경우, 단절점 O
                 if(lists[k].size() >= 2){
                     bw.write("yes\n");
+                // 아닌 경우, 단절점 X    
                 }else {
                     bw.write("no\n");
                 }
             }
             // k번째 간선이 단절선인지 체크
             else {
+                // 현재 문제 조건이 트리인 경우이므로, 모든 간선이 단절선
                 bw.write("yes\n");
             }
         }
