69599999e9fd8b65ce00586d7c82581be2f4cfe6
김경미 <109158497+kyum-q@users.noreply.github.com>
2023-10-21 18:55:52 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 180 ms, Memory: 16148 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -16,19 +16,29 @@ public class Main {
             nums[i] = Integer.parseInt(st.nextToken());
         }
 
+        // nodes 배열은 nums[i]와 i부터 수열을 시작했을 때 최장 수열 len을 기록
         ArrayList<Node> nodes = new ArrayList<>();
+
+        // 뒤에서부터 최장 수열 길이 확인
         for (int i = N - 1; i >= 0; i--) {
             int len = 0;
+            
+            // nodes에 있는 len 값들을 통해 현재 값의 최장 수열 길이 알아내기
             for (int j = nodes.size() - 1; j >= 0; j--) {
                 Node node = nodes.get(j);
+                
+                // 만약 현재 수가 해당 node의 수보다 크고 len도 해당 수보다 클 경우 -> Len 변경
                 if (node.num > nums[i] && len < node.len)
                     len = node.len;
             }
+            // nodes 배열에 추가
             nodes.add(new Node(nums[i], len + 1));
         }
 
+        // nodes 배열을 len 길이 순으로 내림차순 나열
         Collections.sort(nodes);
 
+        // 가장 큰 len 출력
         int result = nodes.get(0).len;
 
         bw.write(result + "\n");
