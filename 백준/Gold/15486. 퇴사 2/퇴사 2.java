29fdf3dc1ca0dab6cf9575f2cbaaff3e6bfc2a22
김경미 <109158497+kyum-q@users.noreply.github.com>
2023-10-23 02:13:11 +0900

    [Gold V] Title: 퇴사 2, Time: 704 ms, Memory: 304596 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -14,43 +13,29 @@ public class Main {
         // dp 값
         int [] dp = new int[N+2];
 
-        // 상담 일정 Array
-        Talk [] talks = new Talk[N+2];
+        int max = 0;
 
         // N일에 따른 상담 일정
         StringTokenizer st;
+
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
 
             int t = Integer.parseInt(st.nextToken());
             int p = Integer.parseInt(st.nextToken());
 
-            talks[i] = new Talk(t, p);
-        }
-
-        int max = 0;
-        for (int i = 1; i < N+2; i++) {
-
             if(max < dp[i])
                 max = dp[i];
 
-            int day = (i>N) ? 0 : i + talks[i].time;
-            int pay = (i>N) ? 0 : talks[i].pay;
-            if (day < N + 2) {
-                dp[day] = Math.max(dp[day], max + pay);
+            if (t+i < N + 2) {
+                dp[t+i] = Math.max(dp[t+i], max + p);
             }
         }
+
+        if(max < dp[N+1])
+            max = dp[N+1];
 
         System.out.println(max);
     }
 
-    public static class Talk {
-        public int time;
-        public int pay;
-        public Talk(int time, int pay) {
-            this.time = time;
-            this.pay = pay;
-        }
-
-    }
 }
