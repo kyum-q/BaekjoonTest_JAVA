952b7e2a5be4ac8fe48e9a1b581786e9dd3ef17a
김경미 <109158497+kyum-q@users.noreply.github.com>
2023-11-15 18:11:33 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 156 ms, Memory: 25,436 KB -BaekjoonHub

---
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.util.stream.Stream;
+import java.io.*;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+
+		//System.setIn(new FileInputStream("res/input.txt"));
+
+
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			int N = sc.nextInt();
+			int K = sc.nextInt();
+			
+			int nums[][] = new int [N+1][N+1];
+			int emptyWidth[][] = new int [N+1][N+1];
+			int emptyHeight[][] = new int [N+1][N+1];
+			
+			int sum = 0;
+			for (int i = 1; i <= N; i++) {
+				for (int j = 1; j <= N; j++) {
+					nums[i][j] = sc.nextInt();
+					if(nums[i][j] == 1) {
+						emptyWidth[i][j] = emptyWidth[i][j-1] + 1;
+						emptyHeight[i][j] = emptyHeight[i-1][j] + 1; 
+					}
+					else {
+						if(emptyWidth[i][j-1] == K) {
+							sum++;
+						}
+						if(emptyHeight[i-1][j] == K) {
+							sum++;
+						}
+					}
+				}
+				if(emptyWidth[i][N] == K) {
+					sum++;
+				}
+			}
+			
+			for (int i = 1; i <= N; i++) {
+				if(emptyHeight[N][i] == K) {
+					sum++;
+				}
+			}
+			
+			System.out.println("#" + test_case + " " + sum);
+			
+		}
+	}
+}
