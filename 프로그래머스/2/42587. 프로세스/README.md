c0a27a2ec2227e38dd85c38f511617c2d9333f4b
김경미 <109158497+kyum-q@users.noreply.github.com>
2024-10-31 00:25:03 +0900

    [level 2] Title: 프로세스, Time: 3.44 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
 
 ### 성능 요약
 
-메모리: 74.2 MB, 시간: 0.64 ms
+메모리: 73.8 MB, 시간: 3.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 6일 22:14:5
+2024년 10월 31일 00:24:59
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,38 +1,37 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[] priorities, int location) {
-        Queue<Process> processes = new LinkedList<>();
-
-        for (int i = 0; i < priorities.length; i++) {
-            processes.add(new Process(i, priorities[i]));
+    public int solution(int[] priorities, int location) {       
+        Queue<Process> process = new LinkedList<>();
+        Queue<Integer> priority = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int i=0;i<priorities.length;i++) {
+            process.add(new Process(i, priorities[i]));
+            priority.add(priorities[i]);
         }
-
-        Arrays.sort(priorities);
-
-        int len = priorities.length;
-        int count = 1;
-        while (!processes.isEmpty()) {
-            Process p = processes.poll();
-
-            while(p.priority != priorities[len-count]) {
-                processes.add(p);
-                p = processes.poll();
+        
+        int result = 0;
+        while(!priority.isEmpty()) {
+            int p = priority.peek();
+            Process pro = process.poll();
+            while(p != pro.priority) {
+                process.add(pro);
+                pro = process.poll();
             }
-
-            if(p.index == location)
-                break;
-
-            count++;
+            priority.poll();
+            result++;
+            if(pro.index == location) {
+                return result;
+            }
         }
-
-        return count;
+        
+        return 0;
     }
-
+    
     public static class Process {
         int index;
         int priority;
-
+        
         public Process(int index, int priority) {
             this.index = index;
             this.priority = priority;
