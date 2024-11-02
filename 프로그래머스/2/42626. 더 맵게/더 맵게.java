45fded42b04ac5f7e1c5ffd8186708f8425ef886
김경미 <109158497+kyum-q@users.noreply.github.com>
2024-11-02 17:15:04 +0900

    [level 2] Title: 더 맵게, Time: 1545.45 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,32 +1,22 @@
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
-        Queue<Integer> scovilles = new PriorityQueue<>();
-
+        Queue<Integer> queue = new PriorityQueue<>();
         for(int s : scoville) {
-            scovilles.add(s);
-        }
-
-        int count = 0;
-        boolean isCheck = false;
-        while (!scovilles.isEmpty()) {
-            int value = scovilles.poll();
-            if(value >= K) {
-                isCheck = true;
-                break;
-            }
-            if(scovilles.isEmpty()) {
-                break;
-            }
-            scovilles.add(value + (scovilles.poll()*2));
-            count++;
+            queue.add(s);
         }
         
-        if(isCheck) {
-            return count;
+        int answer = 0;
+        while(queue.peek() < K) {
+            if(queue.size() < 2) {
+                return -1;
+            }
+            int value = queue.poll();
+            queue.add(value + queue.poll() * 2);
+            answer++;
         }
-        return  -1;
+        
+        return answer;
     }
 }
