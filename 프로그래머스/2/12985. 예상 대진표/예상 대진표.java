b2aa441fcd71896d614fead7df0e84cf2caec84a
김경미 <109158497+kyum-q@users.noreply.github.com>
2024-11-03 17:25:03 +0900

    [level 2] Title: 예상 대진표, Time: 0.05 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int a, int b) {
+        int round = 1;
+        
+        while ((a + 1) / 2 != (b + 1) / 2) {
+            a = (a + 1) / 2; 
+            b = (b + 1) / 2;
+            round++;
+        }
+        
+        return round;
+    }
+}
